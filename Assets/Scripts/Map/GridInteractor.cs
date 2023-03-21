1378110698412fa7bd740f9d5d10aba8948991f8
4MoPDai <vip.russle@gmail.com>
2023-03-21 10:44:23 +0200

    Update - 1.2.1.4 1. Готовлюсь к изменению скрипта полностью - GridInteractor 2. Возможный код Бота - II

--- Assets/Scripts/Map/GridInteractor.cs
+++ Assets/Scripts/Map/GridInteractor.cs
@@ -8,62 +8,6 @@ public class GridInteractor : Grid
 
     private Unit _unitSelected;
 
-    public void ChangeUnitStats(Unit unit, UnitStatus status)
-    {
-        if (status == UnitStatus.Selected)
-        {
-            SelectUnit(unit);
-            SelectCell(unit.CurrentCell, unit.Status);
-            Debug.Log("Unit Selected!");
-        }
-        else
-        {
-            UnselectUnit(unit);
-            UnselectCell(unit.CurrentCell);
-            Debug.Log("Unit Unselected!");
-        }
-    }
-
-    private void SelectUnit(Unit unit)
-    {
-        //unit.Type = UnitType.Player;
-        unit.Status = UnitStatus.Selected;
-
-    }
-
-    public void UnselectUnit(Unit unit)
-    {
-        unit.Status = UnitStatus.Unselected;
-
-    }
-
-    public void SelectCell(Cell cell, UnitStatus status)
-    {
-        if (status == UnitStatus.Selected)
-        {
-            cell.UnitOn = UnitOnStatus.Yes;
-            cell.EUnitState = State.Movement;
-            cell.ChangeColor(cell.CellUnitOnColor);
-        }
-        else
-        {
-            cell.UnitOn = UnitOnStatus.No;
-            cell.EUnitState = State.Selected;
-            cell.ChangeColor(cell.CellStandardColor);
-        }
-
-    }
-
-    public void UnselectCell(Cell cell)
-    {
-        cell.EUnitState = State.Standard;
-        cell.UnitOn = UnitOnStatus.No;
-        cell.ChangeColor(cell.CellStandardColor);
-    }
-
-
-
-    private UnitType CheckUnitType(Unit unit) => unit.Type; // Enemy|Player
 
 }
 
--- Assets/Scripts/PlayerRay.cs
+++ Assets/Scripts/PlayerRay.cs
@@ -9,34 +9,6 @@ public class PlayerRay : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.gameObject.CompareTag("Unit"))
-                {
-                    var unit = hit.collider.gameObject.GetComponent<Unit>();
-                    _gridInteractor.ChangeUnitStats(unit, unit.Status);
-                }
-                foreach (var unit in _gridInteractor.Units)
-                {
-                    if (unit.CurrentCell.Position == hit.collider.GetComponent<Cell>().Position) // unit active
-                    {
-                        if (unit.Status == UnitStatus.Unselected)
-                            _gridInteractor.ChangeUnitStats(unit, UnitStatus.Unselected);
-                        else
-                        _gridInteractor.ChangeUnitStats(unit, UnitStatus.Selected);
-                    }
-                    // Проверка - выбор пустой клетки, при Selected = true;
-                    else if (unit.Status == UnitStatus.Selected)
-                        _gridInteractor.ChangeUnitStats(unit, UnitStatus.Selected);
-                    else
-                        _gridInteractor.ChangeUnitStats(unit, UnitStatus.Unselected);
-                }
-            }
-        }
     }
 }
